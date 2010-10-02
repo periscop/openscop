8105a5a38fc5453a29720e38b33a48ccdacf42c4
Cedric Bastoul <cedric.bastoul@u-psud.fr>
2010-10-02 13:20:29 +0200

    Verbose and single file options to input/output test program

--- tests/openscop_test.c
+++ tests/openscop_test.c
@@ -69,91 +69,146 @@
 #define TEST_DIR    "."      // Directory to scan for OpenScop files
 #define TEST_SUFFIX ".scop"  // Suffix of OpenScop files
 
+
 /**
- * OpenScop test program.
- * This program scans a directory for openscop files and test each of them.
- * A test has four steps:
+ * test_file function
+ * This function tests an onpenscop file. A test has four steps:
  * 1. read the file to raise the data up to OpenScop data structures,
  * 2. dump the data structures to a new OpenScop file,
  * 3. read the generated file,
  * 4. compare the data structures.
  * If everything went well, the data structure of the two scops are the same.
+ * \param input_name The name of the input file.
+ * \param verbose    Verbose option (1 to set, 0 not to set).
+ * \return 1 if the test is successful, 0 otherwise.
  */
-int main(int argc, char * argv[])
+int test_file(char * input_name, int verbose)
 {
   int success = 0;
   int failure = 0;
-  int suffix_length;
+  int equal;
   char * output_name;
   FILE * input_file, * output_file;
-  DIR  * dir;
-  struct dirent * dp;
   openscop_scop_p input_scop;
   openscop_scop_p output_scop;
 
-  if (argc != 1)
+  printf("Testing file %20s... ", input_name); 
+    
+  // Raise the OpenScop file format to OpenScop data structures.
+  input_file = fopen(input_name, "r");
+  if (input_file == NULL)
   {
-    fprintf(stderr, "usage: openscop_test\n");
+    fflush(stdout);
+    fprintf(stderr, "\nError: unable to open file %s\n", input_name);
+    exit(2);
+  }
+  input_scop = openscop_scop_read(input_file);
+  fclose(input_file);
+  
+  // Dump the OpenScop data structures to OpenScop file format.
+  output_name = tmpnam(NULL);
+  output_file = fopen(output_name, "w");
+  openscop_scop_print_dot_scop(output_file, input_scop);
+  fclose(output_file);
+
+  // Raise the generated file to data structures.
+  output_file = fopen(output_name, "r");
+  output_scop = openscop_scop_read(output_file);
+  fclose(output_file);
+
+  if (verbose)
+  {
+    printf("\n\n*************************************************\n\n");
+    openscop_scop_print_dot_scop(stdout, output_scop);
+    printf("\n*************************************************\n\n");
+  }
+
+  // Compare the two scops.
+  if (equal = openscop_scop_equal(input_scop, output_scop))
+    printf("Success :-)\n");
+  else
+    printf("Failure :-(\n");
+
+  // Save the planet.
+  openscop_scop_free(input_scop);
+  openscop_scop_free(output_scop);
+  remove(output_name);
+
+  return equal;
+}
+
+
+/**
+ * OpenScop test program.
+ * Usage: openscop_test [-v] [openscop_file]
+ * This program scans a directory for openscop files and test each of them.
+ * Optionnally the user can provide a file name to check this file only. A
+ * verbose option is also provided to output more information during tests.
+ */
+int main(int argc, char * argv[])
+{
+  int total   = 0; // Total number of tests.
+  int success = 0; // Number of successes.
+  int verbose = 0; // 1 if the verbose option is set, 0 otherwise.
+  int dirtest = 1; // 1 if we check a whole directory, 0 for a single file.
+  int fileidx = 0; // Index of the file to check in argv (0 if none).
+  int suffix_length;
+  DIR * dir;
+  struct dirent * dp;
+
+  // Process the command line information
+  if (((argc > 1) && (!strcmp(argv[1], "-v"))) ||
+      ((argc > 2) && (!strcmp(argv[2], "-v"))))
+    verbose = 1;
+
+  if ((argc > 3) || ((argc == 3) && (!verbose)))
+  {
+    fprintf(stderr, "usage: openscop_test [-v] [openscop_file]\n");
     exit(1); 
   }
 
-  suffix_length = strlen(TEST_SUFFIX);
-  
-  // For each file in the directory to check...
-  dir = opendir(TEST_DIR);
-  while ((dp = readdir(dir)) != NULL)
+  if ((argc - verbose) > 1)
   {
-    // If the file has the convenient suffix...
-    if ((dp->d_namlen > suffix_length) &&
-        (strcmp(dp->d_name+(dp->d_namlen-suffix_length), TEST_SUFFIX) == 0))
-    {
-      printf("Testing file %20s... ", dp->d_name); 
-    
-      // Raise the OpenScop file format to OpenScop data structures.
-      input_file = fopen(dp->d_name, "r");
-      input_scop = openscop_scop_read(input_file);
-      fclose(input_file);
-  
-      // Dump the OpenScop data structures to OpenScop file format.
-      output_name = tmpnam(NULL);
-      output_file = fopen(output_name, "w");
-      openscop_scop_print_dot_scop(output_file, input_scop);
-      fclose(output_file);
-
-      // Raise the generated file to data structures.
-      output_file = fopen(output_name, "r");
-      output_scop = openscop_scop_read(output_file);
-      fclose(output_file);
-
-      // Compare the two scops.
-      if (openscop_scop_equal(input_scop, output_scop) == 1)
-      {
-        printf("Success :-)\n");
-        success++;
-      }
-      else
-      {
-        printf("Failure :-(\n", argv[1]);
-        failure++;
-      }
-
-      /* Save the planet. */
-      openscop_scop_free(input_scop);
-      openscop_scop_free(output_scop);
-      remove(output_name);
-    }
+    dirtest = 0;
+    fileidx = (!strcmp(argv[1], "-v")) ? 2 : 1;
   }
 
-  closedir(dir);
+  // Proceed with the test(s), either directory or single file
+  if (dirtest)
+  {
+    suffix_length = strlen(TEST_SUFFIX);
+    
+    // For each file in the directory to check...
+    dir = opendir(TEST_DIR);
+    while ((dp = readdir(dir)) != NULL)
+    {
+      // If the file has the convenient suffix...
+      if ((dp->d_namlen > suffix_length) &&
+          (!strcmp(dp->d_name+(dp->d_namlen-suffix_length), TEST_SUFFIX)))
+      {
+        // Test it !
+        success += test_file(dp->d_name, verbose);
+        total++;
+      }
+    }
+    closedir(dir);
+  }
+  else
+  {
+    success += test_file(argv[fileidx], verbose);
+    total++;
+  }
+
   printf("\n  +-----------------------+\n");
   printf("  | OpenScop Test Summary |\n");
   printf("  |-----------------------|\n");
-  printf("  | total          %4d   |\n", success+failure);
+  printf("  | total          %4d   |\n", total);
   printf("  | success(es)    %4d   |\n", success);
-  printf("  | failure(s)     %4d   |\n", failure);
+  printf("  | failure(s)     %4d   |\n", total - success);
   printf("  +-----------------------+\n\n");
 
-  if (failure)
+  // Return 0 if all tests were successful, 1 otherwise.
+  if (total - success)
     return 1;
   else
     return 0;
