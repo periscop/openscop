491baeab4a41fca9d2089f8fd88fe9cae29789ed
Lnc <hnc@singularity.fr>
2015-02-25 10:36:20 +0100

    Add osl1_relation_fprint_polylib and osl1_relation_sprint_polylib functions

---
+++ include/osl1/print_polylib.h
@@ -0,0 +1,40 @@
+// Copyright © 2015 Inria, Written by Lénaïc Bagnères, lenaic.bagneres@inria.fr
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#ifndef OSL1_PRINT_POLYLIB_H
+#define OSL1_PRINT_POLYLIB_H
+
+#include <stdio.h>
+
+#include "relation.h"
+
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif
+
+
+// Relation
+void osl1_relation_fprint_polylib(FILE* file,
+                                  const osl1_relation_t* const relation);
+void osl1_relation_sprint_polylib(char** c_str,
+                                  const osl1_relation_t* const relation);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif
