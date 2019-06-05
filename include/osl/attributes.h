746e09c0d68cac9af7a6797b5e4bbd914ca8ad37
Harenome Ranaivoarivony-Razanajato <ranaivoarivony-razanajato@hareno.me>
2019-06-05 18:11:48 +0200

    attributes.h: add some attributes

---
+++ include/osl/attributes.h
@@ -0,0 +1,102 @@
+
+    /*+-----------------------------------------------------------------**
+     **                       OpenScop Library                          **
+     **-----------------------------------------------------------------**
+     **                         attributes.h                            **
+     **-----------------------------------------------------------------**
+     **                   First version: 05/06/2019                     **
+     **-----------------------------------------------------------------**
+
+ 
+ *****************************************************************************
+ * OpenScop: Structures and formats for polyhedral tools to talk together    *
+ *****************************************************************************
+ *    ,___,,_,__,,__,,__,,__,,_,__,,_,__,,__,,___,_,__,,_,__,                *
+ *    /   / /  //  //  //  // /   / /  //  //   / /  // /  /|,_,             *
+ *   /   / /  //  //  //  // /   / /  //  //   / /  // /  / / /\             *
+ *  |~~~|~|~~~|~~~|~~~|~~~|~|~~~|~|~~~|~~~|~~~|~|~~~|~|~~~|/_/  \            *
+ *  | G |C| P | = | L | P |=| = |C| = | = | = |=| = |=| C |\  \ /\           *
+ *  | R |l| o | = | e | l |=| = |a| = | = | = |=| = |=| L | \# \ /\          *
+ *  | A |a| l | = | t | u |=| = |n| = | = | = |=| = |=| o | |\# \  \         *
+ *  | P |n| l | = | s | t |=| = |d| = | = | = | |   |=| o | | \# \  \        *
+ *  | H | | y |   | e | o | | = |l|   |   | = | |   | | G | |  \  \  \       *
+ *  | I | |   |   | e |   | |   | |   |   |   | |   | |   | |   \  \  \      *
+ *  | T | |   |   |   |   | |   | |   |   |   | |   | |   | |    \  \  \     *
+ *  | E | |   |   |   |   | |   | |   |   |   | |   | |   | |     \  \  \    *
+ *  | * |*| * | * | * | * |*| * |*| * | * | * |*| * |*| * | /      \* \  \   *
+ *  | O |p| e | n | S | c |o| p |-| L | i | b |r| a |r| y |/        \  \ /   *
+ *  '---'-'---'---'---'---'-'---'-'---'---'---'-'---'-'---'          '--'    *
+ *                                                                           *
+ * Copyright (C) 2008 University Paris-Sud 11 and INRIA                      *
+ *                                                                           *
+ * (3-clause BSD license)                                                    *
+ * Redistribution and use in source  and binary forms, with or without       *
+ * modification, are permitted provided that the following conditions        *
+ * are met:                                                                  *
+ *                                                                           *
+ * 1. Redistributions of source code must retain the above copyright notice, *
+ *    this list of conditions and the following disclaimer.                  *
+ * 2. Redistributions in binary form must reproduce the above copyright      *
+ *    notice, this list of conditions and the following disclaimer in the    *
+ *    documentation and/or other materials provided with the distribution.   *
+ * 3. The name of the author may not be used to endorse or promote products  *
+ *    derived from this software without specific prior written permission.  *
+ *                                                                           *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR      *
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES *
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.   *
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,          *
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  *
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, *
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY     *
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT       *
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  *
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.         *
+ *                                                                           *
+ * OpenScop Library, a library to manipulate OpenScop formats and data       *
+ * structures. Written by:                                                   *
+ * Cedric Bastoul     <Cedric.Bastoul@u-psud.fr> and                         *
+ * Louis-Noel Pouchet <Louis-Noel.pouchet@inria.fr>                          *
+ *                                                                           *
+ *****************************************************************************/
+
+#ifndef OSL_ATTRIBUTES_H
+#define OSL_ATTRIBUTES_H
+
+#if __GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 96)
+#define OSL_PURE __attribute__((__pure__))
+#define OSL_MALLOC __attribute__((__malloc__))
+#else
+#define OSL_PURE
+#define OSL_MALLOC
+#endif
+
+#if __GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ > 4)
+#define OSL_CONST __attribute__((__const__))
+#define OSL_UNUSED __attribute__((__unused__))
+#else
+#define OSL_CONST
+#define OSL_UNUSED
+#endif
+
+#if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 1)
+#define OSL_DEPRECATED __attribute__((__deprecated__))
+#else
+#define OSL_DEPRECATED
+#endif
+
+#if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 3)
+#define OSL_NONNULL __attribute__((nonnull))
+#define OSL_NONNULL_ARGS(...) __attribute__((nonnull(__VA_ARGS__)))
+#else
+#define OSL_NONNULL
+#define OSL_NONNULL_ARGS(...)
+#endif
+
+#if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4)
+#define OSL_WARN_UNUSED_RESULT __attribute__((warn_unused_result))
+#else
+#define OSL_WARN_UNUSED_RESULT
+#endif
+
+#endif
